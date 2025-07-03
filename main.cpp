2189ee0d207fcab0d5e643355237e9f183c5d5c5
simon <2806926653@qq.com>
2025-07-03 23:28:17 +0800

    添加sort算法函数

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 
+// 冒泡排序
+void bubble_sort(int array[], int n)
+{
+	for (int i = 0; i < n-1; i ++)
+	{
+		for (int j = 0; j < n-i-1; j++)
+		{
+			if (array[j] > array[j+1])
+			{
+				int temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
+}
+
+void sort_test()
+{
+	int n = 5;
+	int array[] = {2, 10, -22, 33, 7};
+	std::cout << "原始数组：";
+	for (auto item : array)
+	{
+		std::cout << item << “ ”;
+	}
+	std::endl;
+
+	bubble_sort(array, n);
+
+	std::cout << "冒泡排序后：";
+	for (auto item : array)
+	{
+		std::cout << item << " ";
+	}
+	std::endl;
+}
+
 // ubuntu 完成了add方法
 int add(int a, int b)
 {
@@ -10,5 +48,7 @@ int main()
 {
 	// win-simon 对main函数做了注释
 	std::cout << "Hello word!" << std::endl;
+	
+	sort_test();
 	return 0;
 }
